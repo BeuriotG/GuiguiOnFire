d87399c613a23a14443a93b0ffa439328e452955
Guillaume Beuriot <gbeuriot.95@gmail.com>
2024-08-08 18:18:32 +0200

    Feat: "creation of 3 buttons that increment, decrement and reset a counter"

---
+++ script.js
@@ -0,0 +1,23 @@
+let number = 0;
+let numberString = document.getElementById('number');
+numberString.innerHTML = `Le compte est de ${number}`;
+
+
+const btnAdd = document.getElementById('btn-add');
+const btnSub = document.getElementById('btn-sub');
+const btnReset = document.getElementById('btn-reset');
+
+btnAdd.addEventListener('click', () => {
+    number += 1;
+    numberString.innerHTML = `Le compte est de ${number}`;
+});
+
+btnSub.addEventListener('click', () => {
+    number -= 1;
+    numberString.innerHTML = `Le compte est de ${number}`;
+});
+
+btnReset.addEventListener('click', () => {
+    number = 0;
+    numberString.innerHTML = `Le compte est de ${number}`;
+});
